fe3f9e8fe8f29b618df1ac44dfc89313126eae22
Prachii <141548171+Prachii1111@users.noreply.github.com>
2026-06-01 15:43:23 +0530

    Delete Notes App/backend directory

--- Notes App/backend/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./src/app.js"
-import connectDB from "./src/config/database.js"
-
-connectDB();
-
-app.listen(3000, () => {
-    console.log("Server running at localhost 3000");
-})
